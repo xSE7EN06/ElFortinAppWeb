eabd644797b1a12d4269178335cec4c04389faa0
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-12 23:07:15 -0600

    feat: agregar interfaz para restaurante

---
+++ src/app/shared/models/restaurant/restaurant.ts
@@ -0,0 +1,10 @@
+export interface Restaurant {
+    id: string;
+    name: string;
+    location: string;
+    opening_hours: string;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at: Date | null;
+  }
+  
