39e0c307aeef2b8f05385f6ccb8b8713a951e1bb
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-12 22:54:58 -0600

    feat: agregar servicio de proveedores

---
+++ src/app/services/providers/providers.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Provider } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProvidersService {
+  private apiUrl = "https://fortin.christba.com/api/proveedores";
+  
+  constructor(private http: HttpClient) { }
+
+  //Metodo para obtener todos los proveedores
+  getProviders():Observable<Provider[]>{
+  return this.http.get<{ status: string; message: string; data: Provider[] }>(this.apiUrl)
+      .pipe(
+        map(response => response.data)  // Extrae solo el array 'data' de la respuesta
+      );
+  }
+
+  //Metodo para obtener un proveedor por id
+  getProviderById(id:number):Observable<any>{
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+}
