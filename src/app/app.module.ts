ffedcac5f1084ef81402c4ec29944a5ef39ca47e
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-13 13:16:36 -0600

    feat: agregar loader como interceptor para peticiones http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,  HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-
+import { SpinnerInterceptor } from './interceptors/spinner.interceptor';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 @NgModule({
   declarations: [
     AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +19,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     HttpClientModule
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),{ provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true }
   ],
   exports:[],
   bootstrap: [AppComponent]
