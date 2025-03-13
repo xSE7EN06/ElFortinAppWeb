f1b75a456e28f7c12c490619720becdca5f09c0e
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-13 11:51:28 -0600

    feat: crear componente dialog-form
    se creo el componente para el formulario en la ventana emergente al editar un registro

---
+++ src/app/shared/components/dialog-form/dialog-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-form',
+  templateUrl: './dialog-form.component.html',
+  styleUrl: './dialog-form.component.css'
+})
+export class DialogFormComponent {
+
+}
