ce9bebfba2131cd2171fe98fad94bd1570ab8a89
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-13 11:34:12 -0600

    feat: agregar funcion para borrar usuarios

--- src/app/admin/pages/users-page/users-page.component.ts
+++ src/app/admin/pages/users-page/users-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import {User} from '../../../shared/models/user/user'
 import { UsersService } from '../../../services/users/users.service';
-
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../../shared/components/dialog/dialog.component';
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
@@ -11,8 +12,8 @@ import { UsersService } from '../../../services/users/users.service';
 export class UsersPageComponent {
   displayedColumns: string[] = ['id', 'name','email', 'role', 'actions'];
   Users: User[] = [];
+  constructor(private usuarioService: UsersService, private dialog: MatDialog,){}
 
-  constructor(private usuarioService: UsersService){}
 
   ngOnInit(){
     this.getUsuarios();
@@ -33,7 +34,53 @@ export class UsersPageComponent {
   
   }
 
-  delete(user: User) {
- 
+   // Método para eliminar un usuario
+   delete(user: User) {
+    // Mostrar el diálogo de confirmación
+    this.mostrarDialog(
+      '¿Estás seguro de borrar este usuario?',
+      `Se eliminará a ${user.nickname} permanentemente.`,
+      'Eliminar'
+    ).subscribe((result: boolean) => {
+      if (result) {
+        // Si el usuario confirma, llamar al servicio para eliminar
+        this.usuarioService.deleteUsuario(user.id).subscribe({
+          next: () => {
+            this.mostrarDialog(
+              'Usuario Eliminado',
+              ':)',
+              null
+            );
+            this.getUsuarios();
+          },
+          error: (err) => {
+            this.mostrarDialog(
+              'Error al eliminar el usuario',
+              ':(',
+              null
+            );
+          }
+        });
+      } else {
+        this.mostrarDialog(
+          'Accion cancelada ',
+          'Operación cancelada satisfactoriamente',
+          null
+        );
+      }
+    });
   }
+
+  mostrarDialog(tittle:string,message:string,action:string | null){
+   const dialogRef = this.dialog.open(DialogComponent, {
+      data: {
+        title: tittle,
+        message: message,
+        action: action
+      }
+    });
+
+    return dialogRef.afterClosed();
+  }
+
 }
