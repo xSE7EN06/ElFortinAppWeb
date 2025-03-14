a6b11c5368f5ef5ad063f37f4bddcfd3400711ec
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-13 23:28:21 -0600

    feat: agregar funcion de register con la api

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,26 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from '../../shared/models/user/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  apiUrl:string='https://fortin.christba.com/api/login';
+  apiUrlLogin:string='https://fortin.christba.com/api/login';
+  apiUrlRegister:string='https://fortin.christba.com/api/usuarios';
   constructor(private http: HttpClient) { }
 
-  register(user: {  userName:string; password: string;  confirmPassword: string; }) {
-    // Guardamos los datos en localStorage simulando un registro
-    localStorage.setItem('userName', user.userName);
-    localStorage.setItem('userPassword', user.password);
-    localStorage.setItem('userConfirmPassword', user.confirmPassword);
-    
- }
+   // Función para registrar un nuevo usuario
+   register(
+    name: string,
+    email: string,
+    phone: string,
+    userType: string,
+    nickname: string,
+    password: string,
+    image: string
+  ): Observable<User> { // Usa la interfaz User para tipar la respuesta
+    const body = {
+      name,
+      email,
+      phone,
+      user_type: userType, // Asegúrate de que coincida con lo que la API espera
+      nickname,
+      password,
+      image,
+    };
+    return this.http.post<User>(this.apiUrlRegister, body); // Tipar la respuesta como User
+  }
 
     // Función para hacer login
     login(emailOrNickname: string, password: string): Observable<any> {
       const body = { emailOrNickname, password }; // Cuerpo de la solicitud
-      return this.http.post(this.apiUrl, body); // Hacer la solicitud POST
+      return this.http.post(this.apiUrlLogin, body); // Hacer la solicitud POST
     }
   
 
